4368a33b9af5606aa867b7a1c727ed1c5013dd7d
Anumjawaid <anumjawaid99@gmail.com>
2020-07-07 15:26:01 +0500

    folder having the function to fetch data

--- src/Api/index.js
+++ src/Api/index.js
@@ -6,8 +6,16 @@ const url="https://covid19.mathdro.id/api" //api to fetch the data
 
 export const fetchData  = async() =>{ //exporting the function by using export
     try{
-        const response=await axios.get(url) //getting the response from the URl
-        return response;
+        //{ const response=await axios.get(url) }//getting the response from the URl it rerurns everything from the api
+        const {data:{confirmed,deaths,recovered,lastUpdate}}=await axios.get(url)
+    //    ModifiedData={
+    //         confirmed,
+    //         deaths,
+    //         recovered,
+    //         lastUpdate
+        // };//this can be returned
+        return{confirmed,deaths,recovered,lastUpdate}//only returns the mentioned data
+        
 
     }
     catch(error){}
