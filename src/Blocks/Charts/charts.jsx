95f99c8a8ebb6852a4ffb3ed0ec30fc0ecc760a1
Anumjawaid <anumjawaid99@gmail.com>
2020-07-09 13:00:57 +0500

    added charts functionality

--- src/Blocks/Charts/charts.jsx
+++ src/Blocks/Charts/charts.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {fetchdaily} from '../../Api'
+import {Line,Bar} from 'react-chartjs-2'
+import styles from './charts.module.css'
 
 // Ex6 function
 const Charts = () => {
+    const [dailyData,setdailyData]=useState([])
+
+    useEffect(() => {
+        const fetchApi = async () => {
+        setdailyData(await fetchdaily());
+        }
+        console.log(dailyData)
+
+        fetchApi();
+
+    })
+
+    const lineChart = (
+        dailyData.length !=0//if array is 0 otherwise the folowing
+        ?
+        (
+            <Line
+            data={{
+                labels:dailyData.map(({ date}) => date),
+                datasets:[{
+                    data:dailyData.map(({confirmed}) => confirmed),
+                    label :'infected',
+                    borderColor:'#3333ff',
+                    fill:true,
+                },{
+                    data:dailyData.map(({deaths}) => deaths),
+                    label :'deaths',
+                    backgroundColor:'rgba(255,0,0,0.5)',
+                    borderColor:'#3333ff',
+                    fill:true,
+
+                }],
+            }}            
+            />
+        ):null
+    );
     return(
-        <h1>Charts</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
 
 }
