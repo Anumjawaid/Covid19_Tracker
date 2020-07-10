77f12e78f89b5bef7a3cfcdca62c3c353546eb06
Anumjawaid <anumjawaid99@gmail.com>
2020-07-10 12:26:38 +0500

    Final call

--- src/App.js
+++ src/App.js
@@ -6,23 +6,35 @@ import { fetchData } from './Api'
 
 class App extends React.Component {
   state={
-    data:{}
+    data:{},
+    country:''
 
   }
   async componentDidMount(){
     const fedata=await fetchData() //for an async function using await and storing in variable
-    console.log(fedata)            //calling the variable in console
+    // console.log(fedata)            //calling the variable in console
 
     this.setState({data:fedata})
   }
+  handleCountryChange=async(country)=>{
+    const fedata=await fetchData(country)//country to show data
+    console.log(country)
+    console.log(fedata)
+    this.setState({data:fedata,country:country})
+
+    
+
+  }
   render() 
   {
-    const {data}=this.state
+    const {data,country}=this.state
     return (
       <div className={styles.container}>
+        <h1>Covid_19 Tracker</h1>
         <Cards data={data}/>
-        <Charts />
-        <Country />
+        <Country handleCountryChange={this.handleCountryChange} />
+
+         <Charts data={data} country={country}/>
 
 
       </div>
