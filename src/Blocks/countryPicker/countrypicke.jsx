baba0ab40ee1c922d190678ac389ba05b0fb61a2
Anumjawaid <anumjawaid99@gmail.com>
2020-07-10 12:32:02 +0500

    updating to show every country

--- src/Blocks/countryPicker/countrypicke.jsx
+++ src/Blocks/countryPicker/countrypicke.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {NativeSelect,FormControl} from '@material-ui/core'
+import styles from './countrypick.module.css'
+import {fetchCountries} from '../../Api'
 
 // Ex6 function
-const Country = () => {
+const Country = ({handleCountryChange}) => {
+    const [fetchCountry,setFetchCountry]=useState([])
+    useEffect(()=>{
+        const getcountry=async()=>{
+            setFetchCountry(await fetchCountries())
+        }
+        getcountry()
+    },[setFetchCountry])
     return(
-        <h1>Country</h1>
+        <FormControl className={styles.formcontrol}>
+            <NativeSelect defaultValue=" " onChange={(e)=>handleCountryChange(e.target.value)}>
+                <option value='global'>Global</option>
+                {fetchCountry.map((country,i)=><option value={country} key={i}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 
 }
